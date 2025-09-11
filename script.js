4c065321fdc15116817e2aee48551bdc26dcb728
niharikavermaa01 <niharikaverma01k>
2025-09-11 14:12:42 +0530

    SKill section added

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-const skillsLink = document.getElementsByClassName('.skills-grid');
-const skillsSection = document.getElementsByClassName('skills');
+const skillsPanel = document.querySelector(".skills");
+const closeBtn = document.querySelector(".crossbtn");
+const body = document.querySelector("body");
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-skillsLink.addEventListener('click', function(event) {
-  event.preventDefault(); // prevent default jump
-  skillsSection.scrollIntoView({ behavior: 'smooth' }); // smooth scroll
-  document.body.classList.add('highlight-skills'); // dim other sections
+// Open when navbar "Skills" is clicked
+document.querySelector('.nav-links a[href="#skills"]').addEventListener("click", function(e) {
+  e.preventDefault();
+  skillsPanel.classList.add("active");
+});
+
+closeBtn.addEventListener("click", function() {
+  skillsPanel.classList.remove("active");
+});
+
+// remove skill panel when click scroll
+window.addEventListener("scroll", () => {
+  if (skillsPanel.classList.contains("active")) {
+    skillsPanel.classList.remove("active");
+  }
+});
+
+// responsiveness
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("open");
+});
+
+document.querySelectorAll(".nav-links li a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    hamburger.classList.remove("open");
+  });
 });
